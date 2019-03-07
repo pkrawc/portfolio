dd91a38b56c676bce665df15c3949327c7031259
Patrick Krawczykowski <pkrawz@gmail.com>
2019-03-07 11:18:43 -0600

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,19 @@ import React from "react"
 import { default as NextApp, Container } from "next/app"
 import Head from "next/head"
 import GlobalStyles from "components/global"
+import ReactGA from "react-ga"
 
 export default class App extends NextApp {
   static async getInitialProps({ Component, ctx }) {
     return Component.getInitialProps ? { initialProps: await Component.getInitialProps(ctx) } : {}
+  }
+
+  componentDidMount() {
+    if (!window.analytics) {
+      ReactGA.initialize("UA-112946294-4")
+      window.analytics = true
+    }
+    ReactGA.pageview(window.location.pathname + window.location.search)
   }
 
   render() {
